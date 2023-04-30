479bef5ca46c0e179ea63f5334e2671ca26b871b
Chiratna <prinusuva7876@gmail.com>
2023-04-30 21:46:17 +0530

    feat: changes phase1 from design

--- components/sections/Phase1form.tsx
+++ components/sections/Phase1form.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AirdropBG from "../../public/images/Airdrop.webp";
 const Airdrop = () => {
   return (
     <section
@@ -9,21 +10,23 @@ const Airdrop = () => {
         {/* <div className="absolute flex-row-reverse hidden w-full md:flex">
           <Image src={howItWorksSVG} height={710} alt="" />
         </div> */}
-        <div className="flex flex-col items-center w-full h-full sm:justify-center ">
+        <div className="flex flex-col items-center w-full h-full sm:justify-center">
           <div className="flex flex-col items-center justify-center w-full">
-            <h2 className="my-6 text-xl font-bold text-center text-white md:text-4xl">
-              Please register for $TRAM token phase 1 airdrop{" "}
-            </h2>
-          </div>
-          <div className="relative w-[80%] h-[30vh] lg:h-[50vh] ">
             <Image
-              src="/images/Long_Logo.png"
+              alt=""
+              src={AirdropBG}
               fill
-              alt="icon"
-              className="rounded-xl"
+              className="w-full max-w-[500px] max-h-[700px] hidden md:block aspect-square mr-auto"
             />
+            <h2 className="my-6 text-2xl font-bold text-center text-white md:text-[40px]">
+              Please register for
+            </h2>
+            <span className="mb-6 text-xl text-white md:text-[30px]">
+              $TRAM token phase 1 airdrop
+            </span>
           </div>
-          <div className="w-full max-w-3xl mt-6">
+
+          <div className="w-full  mt-[10vh] z-[99]">
             <iframe
               src="https://docs.google.com/forms/d/e/1FAIpQLScq29QD7FhuUjhiaicRbPaNnHlm8EfUrgXctSj54rfC3dIxsQ/viewform?embedded=true"
               width="100%"
@@ -32,6 +35,12 @@ const Airdrop = () => {
               Form Loading....
             </iframe>
           </div>
+          <Image
+            alt=""
+            src={AirdropBG}
+            fill
+            className="w-full max-w-[500px] max-h-[700px] hidden md:block aspect-square ml-auto mt-auto -translate-x-[5vw] translate-y-[20vh]"
+          />
         </div>
       </div>
     </section>
